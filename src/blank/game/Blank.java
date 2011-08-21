5e95094ac62fb8a43f3ab0364e1c53d9930850f5
kernelpanix <spam.dbellingroth@gmail.com>
2011-08-21 22:45:17 +0200

    Test

--- src/blank/game/Blank.java
+++ src/blank/game/Blank.java
@@ -2,11 +2,16 @@ package blank.game;
  
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
  
 public class Blank extends BasicGame{
+	
+	private Image image;
+	private Graphics img;
  
     public Blank()
     {
@@ -16,7 +21,8 @@ public class Blank extends BasicGame{
     @Override
     public void init(GameContainer gc) 
 			throws SlickException {
- 
+    	image = new Image(800,600);
+    	img = image.getGraphics();
     }
  
     @Override
@@ -29,7 +35,12 @@ public class Blank extends BasicGame{
     public void render(GameContainer gc, Graphics g) 
 			throws SlickException 
     {
- 
+    	img.setBackground(new Color(0,0,0,0));
+    	img.clear();
+    	img.setColor(Color.green);
+    	img.drawLine((int)(Math.random()*800),(int)(Math.random()*600),(int)(Math.random()*800),(int)(Math.random()*600));
+    	img.flush();
+    	image.draw();
     }
  
     public static void main(String[] args) 
@@ -39,6 +50,7 @@ public class Blank extends BasicGame{
 			new AppGameContainer(new Blank());
  
          app.setDisplayMode(800, 600, false);
+         app.setShowFPS(false);
          app.start();
     }
 }
