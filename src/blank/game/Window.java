186ad98acab257af779c700a0d7d50355e380471
kernelpanix <spam.dbellingroth@gmail.com>
2011-08-23 11:28:35 +0200

    Bild eingefügt und gerendert

--- src/blank/game/Window.java
+++ src/blank/game/Window.java
@@ -1,20 +1,31 @@
 package blank.game;
 
+import java.io.IOException;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class Window {
 	
-	private int i = 0;
+	private double i = 0;
 	private long fps,lastFPS,lastFrame;
+	private String title;
+	
+	private Texture megaman;
 	
 	public Window(String title, int width, int height) {
+		this.title = title;
 		try {
 		    Display.setDisplayMode(new DisplayMode(width, height));
 		    Display.setFullscreen(true);
+		    Display.setVSyncEnabled(true);
 		    Display.setTitle(title);
 		    Display.create();
 		} catch (LWJGLException e) {
@@ -22,10 +33,25 @@ public class Window {
 		    System.exit(0);
 		}
 
+		
+		
+		try {
+			megaman = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/megaman.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		// init OpenGL here
 		
 		// enable textures since we're going to use these for our sprites
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		
+		// enable alpha blending
+		// Damit man auch Transparente Texturen haben kann...
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 					
 		// disable the OpenGL depth test since we're rendering 2D graphics
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
@@ -47,8 +73,10 @@ public class Window {
 			
 			render();
 			
+			updateFPS();
+			
 		    Display.update();
-		    Display.sync(60); // cap fps to 60fps
+		    Display.sync(100); // cap fps to 60fps
 
 
 		}
@@ -57,23 +85,29 @@ public class Window {
 	}
 	
 	private void render() {
-		// set the color of the quad (R,G,B,A)
-		GL11.glColor3f(0.5f,0.5f,1.0f);
-			
-		// draw quad
+		
+		Color.white.bind();
+		megaman.bind(); // or GL11.glBind(texture.getTextureID());
+		
+		
+		i+=0.01;
+		
 		GL11.glBegin(GL11.GL_QUADS);
-		    GL11.glVertex2f(i+100,100);
-		    GL11.glVertex2f(i+100+200,100);
-		    GL11.glVertex2f(i+100+200,100+200);
-		    GL11.glVertex2f(i+100,100+200);
+			GL11.glTexCoord2f(0,0);
+			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400,(int)Math.round(Math.cos(i)*200)+300);
+			GL11.glTexCoord2f(1,0);
+			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400+megaman.getTextureWidth(),(int)Math.round(Math.cos(i)*200)+300);
+			GL11.glTexCoord2f(1,1);
+			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400+megaman.getTextureWidth(),(int)Math.round(Math.cos(i)*200)+300+megaman.getTextureHeight());
+			GL11.glTexCoord2f(0,1);
+			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400,(int)Math.round(Math.cos(i)*200)+300+megaman.getTextureHeight());
 		GL11.glEnd();
-		i++;
-		updateFPS();
+		
 	}
 	
 	public void updateFPS() {
 	    if (getTime() - lastFPS > 1000) {
-	        Display.setTitle("blank    FPS: " + fps); 
+	        Display.setTitle(title+"    FPS: " + fps); 
 	        fps = 0; //reset the FPS counter
 	        lastFPS += 1000; //add one second
 	    }
