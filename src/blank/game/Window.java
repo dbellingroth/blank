ef2515bdca8401ee67581d818672752b6ec92a6b
kernelpanix <spam.dbellingroth@gmail.com>
2011-08-23 13:30:30 +0200

    Bild rotiert und bewegt sich... Framerate erhöht...

--- src/blank/game/Window.java
+++ src/blank/game/Window.java
@@ -25,8 +25,8 @@ public class Window {
 		this.title = title;
 		try {
 		    Display.setDisplayMode(new DisplayMode(width, height));
-		    Display.setFullscreen(true);
-		    Display.setVSyncEnabled(true);
+		    //Display.setFullscreen(true);
+		    //Display.setVSyncEnabled(true);
 		    Display.setTitle(title);
 		    Display.create();
 		} catch (LWJGLException e) {
@@ -46,23 +46,27 @@ public class Window {
 		
 		// init OpenGL here
 		
-		// enable textures since we're going to use these for our sprites
-		GL11.glEnable(GL11.GL_TEXTURE_2D);
-		
-		// enable alpha blending
-		// Damit man auch Transparente Texturen haben kann...
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);               
+        
+		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);          
+        
+        // enable alpha blending
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        
+        GL11.glViewport(0,0,width,height);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, width, height, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 					
 		// disable the OpenGL depth test since we're rendering 2D graphics
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
-					
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-					
-		GL11.glOrtho(0, width, height, 0, -1, 1);
 		
 		lastFPS = getTime();
+		delta = getDelta();
 		
 		
 		while (!Display.isCloseRequested()) {
@@ -71,7 +75,7 @@ public class Window {
 			
 		    // render OpenGL here
 			// clear screen
-			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 			GL11.glMatrixMode(GL11.GL_MODELVIEW);
 			GL11.glLoadIdentity();	
 			
@@ -80,7 +84,8 @@ public class Window {
 			updateFPS();
 			
 		    Display.update();
-		    //Display.sync(60); // cap fps to 60fps
+
+		    Display.sync(200); // cap fps to 60fps
 
 
 		}
@@ -92,26 +97,31 @@ public class Window {
 		
 		Color.white.bind();
 		megaman.bind(); // or GL11.glBind(texture.getTextureID());
+
+		i = (i + 0.3*delta)%500;
 		
-		System.out.println(delta);
-		i+=0.001*delta;
+		GL11.glPushMatrix();
+		GL11.glTranslated(400,i,0);
+		GL11.glRotatef((float) i, 0f, 0f, 1f);
 		
 		GL11.glBegin(GL11.GL_QUADS);
 			GL11.glTexCoord2f(0,0);
-			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400,(int)Math.round(Math.cos(i)*200)+300);
+			GL11.glVertex2f(0,0);
 			GL11.glTexCoord2f(1,0);
-			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400+megaman.getTextureWidth(),(int)Math.round(Math.cos(i)*200)+300);
+			GL11.glVertex2f(megaman.getTextureWidth(),0);
 			GL11.glTexCoord2f(1,1);
-			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400+megaman.getTextureWidth(),(int)Math.round(Math.cos(i)*200)+300+megaman.getTextureHeight());
+			GL11.glVertex2f(megaman.getTextureWidth(),megaman.getTextureHeight());
 			GL11.glTexCoord2f(0,1);
-			GL11.glVertex2f((int)Math.round(Math.sin(i)*200)+400,(int)Math.round(Math.cos(i)*200)+300+megaman.getTextureHeight());
+			GL11.glVertex2f(0,megaman.getTextureHeight());
 		GL11.glEnd();
+		
+		GL11.glPopMatrix();
 		
 	}
 	
 	public void updateFPS() {
 	    if (getTime() - lastFPS > 1000) {
-	        Display.setTitle(title+"    FPS: " + fps); 
+	        Display.setTitle(title+" running on "+Display.getAdapter()+"    FPS: " + fps); 
 	        fps = 0; //reset the FPS counter
 	        lastFPS += 1000; //add one second
 	    }
@@ -130,8 +140,7 @@ public class Window {
 	public int getDelta() {
 	    long time = getTime();
 	    int delta = (int) (time - lastFrame);
-	    lastFrame = time;
-	    	
+	    lastFrame = time;	
 	    return delta;
 	}
 	
