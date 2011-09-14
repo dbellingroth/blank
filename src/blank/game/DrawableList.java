2a6e79f2acc02afc25fe5f3832125424f3e5ba67
Kilian177 <KILY100@aol.com>
2011-09-14 23:20:34 +0200

    Liste sortiert beim hinzufügen, kann die Objekte wieder entfernen und ruft auf alle Listenobjekte die draw()-Methode auf

--- src/blank/game/DrawableList.java
+++ src/blank/game/DrawableList.java
@@ -1,10 +1,94 @@
 package blank.game;
 
+import java.util.LinkedList;
+
 public class DrawableList implements Drawable {
 
-	@Override
-	public void draw() {
+	private LinkedList<Drawable> drawables;
+	private int zIndex;
+	
+	
+	
+	
+	DrawableList() {
+		
+		drawables = new LinkedList<Drawable>();
 		
 	}
+	
+	
+	
+	
+	/**
+	 * setzt das neue Drwbl auf den richtigen Index, sortiert nach dem ZIndex
+	 */
+	public void addDrawable(Drawable drawable) {
+		
+		for (int x = 0; x < drawables.size(); x++) {
+			
+			
+			if (drawable.getZIndex() < drawables.get(x).getZIndex()) {
+				drawables.add(x, drawable);
+				break;
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	
+	/**
+	 * löscht das im Parameter angegebene Objekt aus der Liste
+	 * @param drawable
+	 */
+	public void deleteDrawable(Drawable drawable) {
+		
+		drawables.remove(drawable);
+		
+	}
+	
+	
+	
+	
+	/**
+	 * löscht den im Parameter angegebenen Index
+	 * @param index
+	 */
+	public void deleteDrawable(int index) {
+		
+		drawables.remove(index);
+		
+	}
+	
+	
+	
+	
+	/**
+	 * ruft auf alle Objekete der Liste die draw()-Methode auf
+	 */
+	public void draw() {
+		
+		for(int i = 0; i < drawables.size(); i++) {
+			
+			drawables.get(i).draw();
+			
+		}
+		
+	}
+	
+	
+	
 
+	
+	
+	public int getZIndex() {
+		return zIndex;
+	}
+	
+	public void setZIndex(int zIndex) {
+		this.zIndex = zIndex;
+	}
+	
 }
