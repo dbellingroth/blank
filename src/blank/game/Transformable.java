f0cf97930c26bd25561c40c4ba415ad4e0687667
David Bellingroth <david@Davids-MacBook-Pro-2.local>
2011-09-14 23:10:00 +0200

    Transformable implementiert und noch nicht getestet ;)

--- src/blank/game/Transformable.java
+++ src/blank/game/Transformable.java
@@ -1,5 +1,25 @@
 package blank.game;
 
+import java.awt.geom.Point2D;
+
+import org.lwjgl.opengl.GL11;
+
 public abstract class Transformable implements Drawable{
+
+	private Point2D translate = new Point2D.Double(0,0);
+	private Point2D rotationPoint = new Point2D.Double(0,0);
+	private double rotationAngle = 0;
+	private Point2D scaleFactor = new Point2D.Double(1,1);
+	
+	
+	protected void transform() {
+		GL11.glTranslated(translate.getX(),translate.getY(),0);
+		GL11.glScaled(scaleFactor.getX(), scaleFactor.getY(), 1);
+		if (rotationAngle != 0) { //falls Rotation gewünscht
+			GL11.glTranslated(rotationPoint.getX(),rotationPoint.getY(),0);
+			GL11.glRotatef((float)rotationAngle, 0f, 0f, 1);
+			GL11.glTranslated(-rotationPoint.getX(),-rotationPoint.getY(),0);
+		}
+	}
 	
 }
