3442cab6efeb6d4326e659ea1762cf11a7aa731b
kernelpanix <kernelpanix@kernelpanix.de>
2011-09-18 22:55:23 +0200

    Unnötige alte Klassen gelöscht

--- src/blank/trash/GameObject.java
+++
@@ -1,9 +0,0 @@
-package blank.trash;
-
-public interface GameObject {
-
-	
-	public void update(long delta);
-	
-	
-}
--- src/blank/trash/WantDraw.java
+++
@@ -1,9 +0,0 @@
-package blank.trash;
-
-public interface WantDraw {
-
-	
-	public void draw();
-	
-	
-}
