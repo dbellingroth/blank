5c58d7acc7e9be5150b75c19a37843d124b80ff7
kernelpanix <kernelpanix@kernelpanix.de>
2011-09-21 11:20:35 +0200

    Neue Methode entwickelt, wie andere Threads Code im Hauptthread ausführen können. Man fügt mittel Game.addAction einen Executor hinzu und Game führt diesen dann aus. Wurde notwendig, weil ich gemerkt habe, dass man OpenGL Befehle nur in dem Thread benutzen kann in dem man auch den OpenGL Kontext erzeugt hat.
    Neuer Test, Hart getroffene bälle färben ich weiß...
    
    Wir müssen das alles nochmal durchsprechen...

---
+++ src/blank/game/Executor.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package blank.game;
+
+/**
+ * @author David Bellingroth
+ * Wird genutzt um Aktionen, die ausgeführt werden sollen als Parameter übergeben zu können.
+ */
+public abstract class Executor {
+	public abstract void execute();
+}
