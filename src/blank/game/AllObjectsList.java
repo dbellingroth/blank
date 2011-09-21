2c8b69ca41bcc57b0e81e9ab0352dd952fe96976
Kilian177 <KILY100@aol.com>
2011-09-22 00:10:39 +0200

    Liste mit allen Objekten hinzugefügt (GameObjects&Drawables)

---
+++ src/blank/game/AllObjectsList.java
@@ -0,0 +1,68 @@
+package blank.game;
+
+import java.util.ArrayList;
+
+import blank.game.rendering.Drawable;
+import blank.game.rendering.DrawableList;
+
+public class AllObjectsList {
+
+	
+	ArrayList<GameObject> gameObjects;
+	DrawableList drawers;
+	
+	
+	AllObjectsList() {
+		
+		gameObjects = new ArrayList<GameObject>();
+		drawers = new DrawableList();
+		
+	}
+	
+	
+	public void add(GameObject go) {
+		
+		if (!gameObjects.contains(go)) {
+			gameObjects.add(go);	
+		}
+		
+		if (go instanceof Drawable) {
+			if (!drawers.contains((Drawable) go)) {
+				drawers.addDrawable((Drawable) go);
+			}
+		}
+	}
+	
+	
+	public void removeGameObject(GameObject go) {
+		
+		if (gameObjects.contains(go)) {
+			gameObjects.remove(go);			
+		}
+		
+		if (go instanceof Drawable) {
+			if (drawers.contains((Drawable) go)) {
+				drawers.removeDrawable((Drawable) go);
+			}
+		}
+		
+	}
+	
+	
+	public void update(int delta) {
+		
+		for(GameObject go : gameObjects) {
+			go.update(delta);
+		}
+		
+	}
+	
+	
+	public void draw() {
+		
+		drawers.draw();
+		
+	}
+	
+	
+}
