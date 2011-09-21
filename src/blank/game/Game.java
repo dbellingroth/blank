b4dfabeecff3c1b22cbea857a8f480099648e56e
Kilian177 <KILY100@aol.com>
2011-09-22 00:11:36 +0200

    Test-Rectangle ist jetzt ein DynamicRectangle, auf AllObjectsList umgestiegen

--- src/blank/game/Game.java
+++ src/blank/game/Game.java
@@ -1,17 +1,14 @@
 package blank.game;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
+
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.Semaphore;
-
-import org.jbox2d.common.Vec2;
-
-import blank.game.physics.PhysicsBox;
 import blank.game.physics.PhysicsStaticBlock;
 import blank.game.physics.PhysicsWorld;
-import blank.game.rendering.Sprite;
+
+
 /**
  * Die eigentliche Hauptklasse mit der Mainmethode.
  * @author Kilian Helmenstein, David Bellingroth
@@ -19,16 +16,14 @@ import blank.game.rendering.Sprite;
  */
 public class Game {
 
-	Sprite test,test2;
-	
-	Sprite trunk, branch;
-	
+
 	private static PhysicsWorld world;
-	private PhysicsBox rect;
-	private LinkedList<Ball> balls;
 	private static Queue<Executor> actionList = new LinkedList<Executor>(); //Qeue von Executors, die ausgeführt werden sollen
 	private static Semaphore actionSem = new Semaphore(1);
 
+	private AllObjectsList allObjects;
+	
+	
 	public static void main(String args[]) {
 		new Window(new Game(), 800, 600);
 	}
@@ -49,28 +44,24 @@ public class Game {
 		
 		world = new PhysicsWorld();
 		
-		rect = new PhysicsBox(105,300,60,60);
-		world.addObject(rect);
 		
+		allObjects = new AllObjectsList();
+		
+	
 		world.addObject(new PhysicsStaticBlock(0,600,800,100));
 		world.addObject(new PhysicsStaticBlock(0,-100,800,100));
 		world.addObject(new PhysicsStaticBlock(-100,0,100,600));
 		world.addObject(new PhysicsStaticBlock(800,0,100,600));
 				
-		test2 = new Sprite(60, 60);
-		Graphics2D g2d = test2.getGraphics2D();
-		g2d.setColor(Color.MAGENTA);
-		g2d.fillRect(0, 0, 60, 60);
-		test2.update();
-		
-		balls = new LinkedList<Ball>();
+//		Bälle hinzufügen
 		for (int i = 1; i< 40; i++) {
 			for (int j = 1; j<30; j++) {
-				balls.add(new Ball(i*20, j*20,(int)(Math.random()*6+4)));
+				allObjects.add(new Ball(i*20, j*20,(int)(Math.random()*6+4)));
 			}
 		}
 		
-		
+//		das Test-Rechteck hinzufügen
+		allObjects.add(new DynamicRectangle(105,300,40,48));
 		
 		world.start();
 	
@@ -83,25 +74,19 @@ public class Game {
 	protected void update(int delta) {
 		
 		executeActions();
-		
-		test2.setTranslate(new Vec2(rect.getPosition().x-30,rect.getPosition().y-30));
-		test2.setRotationPoint(new Vec2(30,30));
-		test2.setRotationAngle(rect.getAngle());
-		
-		//rect.applyAngularImpulse(3f);
-		rect.applyForce(new Vec2(-5000+(float)Math.random()*10000,-5000+(float)Math.random()*10000), new Vec2(25,25));
-		
+			
 		//balls.get((int)(Math.random()*(balls.size()-1)+0.9999)).getPhysicsObject().applyForce(new Vec2(-100+(float)Math.random()*200,-100+(float)Math.random()*200), new Vec2(5,5));
+		
+		allObjects.update(delta);
+		
 		
 		render();
 	}
 	
 	private void render() {
-		test2.draw();
 		
-		for (Ball b : balls) {
-			b.draw();
-		}
+		allObjects.draw();
+		
 	}
 	
 	public void stop() {
