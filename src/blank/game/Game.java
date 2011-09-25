2740ac99773f0c17f2d38634fc8e563846130df1
Kilian177 <KILY100@aol.com>
2011-09-25 22:28:57 +0200

    Das Polygon wird jetzt IM UHRZEIGERSINN gezeichnet!!!

--- src/blank/game/Game.java
+++ src/blank/game/Game.java
@@ -77,10 +77,8 @@ public class Game {
 //		Test-Polygon hinzufügen
 		ArrayList<Vec2> positions = new ArrayList<Vec2>();
 			positions.add(new Vec2(0, 50));
-		
 			positions.add(new Vec2(25, 0));
 			positions.add(new Vec2(50, 50));
-				
 		allObjects.add(new DynamicPolygon(250, 250, positions));
 		
 		
