461c3f8da851fcc5dd1ab4d8405a429f6b6740fd
Kilian177 <KILY100@aol.com>
2011-10-19 19:33:51 +0200

    Camera-Limit hinzugefügt

--- src/blank/game/rendering/Camera.java
+++ src/blank/game/rendering/Camera.java
@@ -13,17 +13,25 @@ import org.lwjgl.opengl.GL11;
 public class Camera {
 
 	private Vec2 position;
+	
+	private Vec2 upper_limit, lower_limit;
+	private boolean enableLimit;
+	
 	private float rotation = 0f;
 	private double zoomfactor = 1f;
 
 	public Camera(Vec2 position) {
+		this.position = position;
+		lower_limit = new Vec2(100, 100);
+		upper_limit = new Vec2(700, 600);
+		enableLimit = true;
 		setX(position.x);
 		setY(position.y);
 	}
 
 	public void apply() {
 
-		if (rotation != 0f) { // falls Rotation gew�nscht
+		if (rotation != 0f) { // falls Rotation gewünscht
 			GL11.glTranslated(Display.getDisplayMode().getWidth() / 2, Display
 					.getDisplayMode().getHeight() / 2, 0);
 			GL11.glRotatef(-rotation, 0f, 0f, 1f);
@@ -31,7 +39,7 @@ public class Camera {
 					-Display.getDisplayMode().getHeight() / 2, 0);
 		}
 
-		if (zoomfactor != 1) { // falls Rotation gew�nscht
+		if (zoomfactor != 1) { // falls Rotation gewünscht
 			GL11.glTranslated(Display.getDisplayMode().getWidth() / 2, Display
 					.getDisplayMode().getHeight() / 2, 0);
 			GL11.glScaled(zoomfactor, zoomfactor, 1);
@@ -39,8 +47,8 @@ public class Camera {
 					-Display.getDisplayMode().getHeight() / 2, 0);
 		}
 
-		GL11.glTranslated(-position.x + Display.getDisplayMode().getWidth() / 2, -position.y
-				+ Display.getDisplayMode().getHeight() / 2, 0);
+		GL11.glTranslated(-position.x + Display.getDisplayMode().getWidth() / 2, 
+				-position.y	+ Display.getDisplayMode().getHeight() / 2, 0);
 
 	}
 
@@ -62,8 +70,27 @@ public class Camera {
 	
 	
 	public void setPosition(Vec2 position) {
-		setX(position.x);
-		setY(position.y);
+		if (enableLimit) {
+			if (position.x <= upper_limit.x && position.x >= lower_limit.x) {
+				setX(position.x);
+			}
+			if (position.y <= upper_limit.y && position.y >= lower_limit.y) {
+				setY(position.y);
+			}
+		} else if (!enableLimit) {
+			setX(position.x);
+			setY(position.y);
+		}
+	}
+	
+	public void lowerLimit(Vec2 lower_limit) {
+		this.lower_limit.x = lower_limit.x;
+		this.lower_limit.y = lower_limit.y;
+	}
+	
+	public void upperLimit(Vec2 upper_limit) {
+		this.upper_limit.x = upper_limit.x;
+		this.upper_limit.y = upper_limit.y;
 	}
 	
 
